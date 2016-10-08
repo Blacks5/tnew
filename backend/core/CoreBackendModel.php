af8bf3c21b7282ef338295af77a11ed056fdc15f
haytoo <466594257@qq.com>
2016-10-08 22:58:04 +0800

    1

---
+++ backend/core/CoreBackendModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 2016/10/8
+ * Time: 22:57
+ * @author 涂鸿 <hayto@foxmail.com>
+ */
+namespace backend\core;
+use yii;
+use common\core\CoreModel;
+class CoreBackendModel extends CoreModel
+{
+
+}
