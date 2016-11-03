0771a323fb9891ac5b5f979aa05563ec70cc2a2c
haytoo <466594257@qq.com>
2016-11-03 17:39:06 +0800

    安全：一个账户同时只能在一处登录

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -14,7 +14,7 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.qq.com',
