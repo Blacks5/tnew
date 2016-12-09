fd2faaf5ca6a126198bc228ae264c887daf904de
haytoo <466594257@qq.com>
2016-12-09 21:21:21 +0800

    增加rule规则

--- common/models/UploadFile.php
+++ common/models/UploadFile.php
@@ -55,7 +55,7 @@ class UploadFile extends BaseUploadFile
     public function rules()
     {
         return [
-            [['pic'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png,jpg,jpeg,mp4', 'maxSize'=>1024*1024*10, 'tooBig'=>'图片过大', 'wrongExtension'=>'图片格式错误', 'checkExtensionByMimeType'=>false], // 最大10m
+            [['pic'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png,jpg,jpeg,mp4', 'maxSize'=>1024*1024*100, 'tooBig'=>'图片过大', 'wrongExtension'=>'图片格式错误', 'checkExtensionByMimeType'=>false], // 最大10m
             [['type'], 'in', 'range'=>['oi_front_id', 'oi_back_id', 'oi_customer', 'oi_front_bank', 'oi_back_bank', 'oi_driving_license_two', 'oi_driving_license_one', 'oi_family_card_two', 'oi_family_card_one']],
             [['oid', 'key'], 'required']
         ];
