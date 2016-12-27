306d76d327b0150487e22b22d83cd1f71d7882cb
haytoo <466594257@qq.com>
2016-12-27 22:52:36 +0800

    删除照片

--- common/models/OrderImages.php
+++ common/models/OrderImages.php
@@ -40,6 +40,7 @@ class OrderImages extends CoreCommonActiveRecord
     {
         return [
             [['oi_front_id', 'oi_back_id', 'oi_customer', 'oi_front_bank'/*, 'oi_back_bank'*/], 'required'],
+            [['oi_pick_goods', 'oi_serial_num'], 'safe'],
             [['oi_front_id', 'oi_back_id', 'oi_customer', 'oi_front_bank'/*, 'oi_back_bank'*/, 'oi_family_card_one', 'oi_family_card_two', 'oi_driving_license_one', 'oi_driving_license_two', 'oi_after_contract', 'oi_video'], 'string', 'max' => 100],
         ];
     }
