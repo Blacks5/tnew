a3a3786f50ddde61e3a945c2b9faf85a2fbb7d7d
bruce-918 <1229297469@qq.com>
2016-12-29 01:07:15 +0800

    测试bug时间

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -2,17 +2,17 @@
 return [
     'components' => [
         'db' => [
-            /*            'class' => 'yii\db\Connection',
-                        'dsn' => 'mysql:host=211.149.163.238;port=4546;dbname=wcb_latest',
-                        'username' => 'haytoo',
-                        'password' => 'hsd89h&hhOH09UH90jo',
-                        'charset' => 'utf8',*/
-
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.0.7;port=4546;dbname=wcb_latest',
-            'username' => 'root',
-            'password' => '123456',
+            'dsn' => 'mysql:host=211.149.163.238;port=4546;dbname=wcb_latest',
+            'username' => 'haytoo',
+            'password' => 'hsd89h&hhOH09UH90jo',
             'charset' => 'utf8',
+
+            /*            'class' => 'yii\db\Connection',
+                        'dsn' => 'mysql:host=192.168.0.7;port=4546;dbname=wcb_latest',
+                        'username' => 'root',
+                        'password' => '123456',
+                        'charset' => 'utf8',*/
             /*'attributes'=>[
                 'PDO::ATTR_TIMEOUT' => 150,
                 'PDO::ATTR_ERRMODE' => PDO::ERRMODE_EXCEPTION
