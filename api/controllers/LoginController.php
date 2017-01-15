c9efa17f280551f2c6d0a9b6ee241729696e7a0d
haytoo <466594257@qq.com>
2017-01-15 22:24:14 +0800

    增加合同内容

--- api/controllers/LoginController.php
+++ api/controllers/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
      */
     public function actionReturnUrl()
     {
-        $url = 'http://211.149.163.238:80/';
+        $url = 'http://211.149.163.238:89/';
         return ['status' => 1, 'message' => 'ok', 'data' => $url];
     }
 }
