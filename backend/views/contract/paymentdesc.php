d09549d664d59d593fe51aeb00483223cd583610
bruce-918 <1229297469@qq.com>
2017-02-16 00:07:01 +0800

    合同页面

--- backend/views/contract/paymentdesc.php
+++ backend/views/contract/paymentdesc.php
@@ -1,10 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-CN">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
+<style>
+    .container {
+        padding: 15px;
+    }
+
+    .w_div {
+        display: block;
+        float: left;
+        width: 100%;
+    }
+
+    dl.info_box dd {
+        margin-bottom: 10px;
+    }
+</style>
 <body>
-我是代扣说明
+<div class="container">
+    <div class="w_div">
+        <h2 class="info_box_dt" style="text-align: center;">还款说明</h2>
+        <p>
+            本人确认一旦提交本借款申请表，即应当在签署本借款申请表后3日内将相关商品的首付金额以银行转账方式支付至上述指定还款账户，否则客户服务供应商有权拒绝接受本人提交的申请并将自付金额按照转账汇款原路径退还，本人确认该等转账汇款账户系本人合法所有并确保其可以接收退款，只要按照转账汇款原路径退还自付金额，即视为自付金额已经向本人退还。</p>
+        <p>
+            本人选择银行代扣还款，表明本人同意并授权出借人深圳天道计然金融服务有限公司（“客户服务供应商”)或美利网运营公司可通过银行从般若指定的银行账户（即以上客户个人账户）中将每月还款额及其他应付款项转入指定还款账户。本人同意此银行代扣授权同时适用于之前所签署的一份或多份借款服务合同，即出借人、客户服务供应商、美利网运营公司可通过银行从容指定的上述银行账户余额中划扣本人在各合同项下应支付给出借人、客户服务供应商、没利网运营公司的相关款项。此外，该账户同时可用于提前清偿等情况引起的资金往来，若上述银行账户无法完成扣款，则本人同意并授权从本人其它银行卡进行代扣以保证本人按时履行每月还款义务。</p>
+    </div>
+</div>
 </body>
 </html>
