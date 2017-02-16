0d99f2ed161b8c6881a2cf34441ec5b61f8c5dd5
haytoo <466594257@qq.com>
2017-02-16 17:10:37 +0800

    修改： 天牛logo id换成编号

--- backend/views/login/login.php
+++ backend/views/login/login.php
@@ -7,6 +7,25 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
+/*问候语句*/
+$time_str = [
+    '卧槽这么晚还上班，必须涨工资啊' =>[0, 6], // 0点到6点
+    '早上好啊，吃过饭没？' =>[7, 8],
+    '现在是上午，请搬砖' =>[9, 11],
+    '中午了，快去吃饭' =>[12, 13],
+    '下午啦，好好搬砖' =>[14, 17],
+    '晚上要好好吃饭哟' =>[18, 19],
+    '一天结束啦，是不是该看看书？' =>[20, 21],
+    '差不多就去睡觉' =>[22, 23],
+];
+$now = (new \DateTime())->format('H');
+$end_str = '遭了，现在几点啦？';
+foreach ($time_str as $key=>$value){
+    if(($now >= reset($value)) && ($now <= end($value))){
+        $end_str = $key;
+        break;
+    }
+}
 
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -17,7 +36,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <h1 class="logo-name">TN</h1>
 
         </div>
-        <h3>欢迎使用</h3>
+        <h3><?=$end_str?></h3>
 
 
         <div class="row">
@@ -39,7 +58,7 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     <div class="footer">
-        <div class="">Copyright &copy; 2016-2017 <a href="#" target="_blank">管理系统</a>
+        <div class="">Copyright &copy; 2016-2017 <a href="#" target="_blank">天牛金融管理系统</a>
         </div>
     </div>
 </div>
