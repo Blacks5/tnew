b5db1b780c31e9a5700facbb8568254e6eccd517
haytoo <466594257@qq.com>
2017-02-21 17:20:56 +0800

    修复 若干问题

--- backend/views/stores/view.php
+++ backend/views/stores/view.php
@@ -7,11 +7,11 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="ibox float-e-margins">
     <div class="ibox-content">
-        <!--        <div class="row">
+                <div class="row">
             <div class="col-sm-3">
-                <a href="<? /*= Yii::$app->getUrlManager()->createUrl(['stores/sales', 'store_id'=>$model->s_id]) */ ?>" class="btn btn-success">销售人员</a>
+                <a href="<?= Yii::$app->getUrlManager()->createUrl(['stores/sales', 'store_id'=>$model->s_id])  ?>" class="btn btn-success">销售人员</a>
             </div>
-        </div>-->
+        </div>
         <div class="hr-line-dashed"></div>
         <div class="form-horizontal m-t" id="signupForm" novalidate="novalidate" action="" method="post">
 
