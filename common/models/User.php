7465a8a8fa62eab040ea97d14695aad4a99f1288
haytoo <466594257@qq.com>
2017-02-21 17:07:01 +0800

    修复 若干问题

--- common/models/User.php
+++ common/models/User.php
@@ -73,7 +73,7 @@ class User extends CoreCommonActiveRecord implements \yii\web\IdentityInterface
             [['password_hash', 'password_reset_token', 'email', 'password_hash_1'], 'string', 'max' => 256, 'min' => 6, 'tooShort'=>'{attribute}至少为6位'],
             [['county', 'city', 'province'], 'string', 'max' => 20],
             ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => '{attribute}已存在', 'on'=>'create'],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => '邮箱已存在', 'on'=>'create'],
+//            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => '邮箱已存在', 'on'=>'create'],
             [['cellphone'], 'match', 'pattern' => '/^1[3|5|8]\d{9}$/', 'message'=>'错误的手机号码'],
 
             [['password_hash_1'], 'compare', 'compareAttribute'=>'password_hash', 'message'=>'两次密码不一致'],
