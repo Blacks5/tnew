7a51d781b2cfbfd301775ef080c37e431e003e3f
haytoo <Xoo594257>
2017-03-11 00:42:16 +0800

    修改订单状态颜色

--- api/controllers/LoginController.php
+++ api/controllers/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
      */
     public function actionLogin()
     {
-        p(1111);
         $request = Yii::$app->getRequest();
         $model = new LoginForm();
         $model->username = $request->post('username');
