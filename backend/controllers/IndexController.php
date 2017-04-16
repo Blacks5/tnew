4b9675774f7a28082ec1e9752024b46d6b7f965d
haytoo <Xoo594257>
2017-04-16 20:48:28 +0800

    增加员工身份证和详细地址

--- backend/controllers/IndexController.php
+++ backend/controllers/IndexController.php
@@ -10,6 +10,8 @@ use common\models\Orders;
 use yii\data\Pagination;
 
 use Yii;
+use yii\db\Expression;
+use yii\db\Query;
 
 //class IndexController extends \yii\web\Controller
 class IndexController extends CoreBackendController
@@ -28,13 +30,30 @@ class IndexController extends CoreBackendController
         $log = Log::find()->limit(Yii::$app->params['page_size'])->orderBy('id desc')->asArray()->all();
         $customer = $this->getCustomers();
         $order = $this->getOrdres();
+
+        $total_borrow_money = number_format($this->getTotalMoney());
+
         return $this->render('welcome',[
             'log' => $log,
             'customer'=>$customer,
-            'order'=>$order
+            'order'=>$order,
+            'total_borrow_money'=>$total_borrow_money
         ]);
     }
 
+
+    /**
+     * 总共借出去的金额
+     * @return float
+     * @author too <hayto@foxmail.com>
+     */
+    private function getTotalMoney()
+    {
+        // select (sum(o_total_price)-sum(o_total_deposit))as total from orders where o_status=10
+        $total_money = (new Query())->select(['total_money'=>new Expression('sum(o_total_price)-sum(o_total_deposit)')])->from(Orders::tableName())
+            ->where(['o_status'=>Orders::STATUS_PAYING])->scalar();
+        return round($total_money, 2);
+    }
     /**
      * 获取客户相关
      * @author 涂鸿 <hayto@foxmail.com>
