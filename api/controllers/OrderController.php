57dc9bdba42857e203cb0bebdc0db16710ef4c62
haytoo <466594257@qq.com>
2017-05-11 09:53:49 +0800

    增加上传特效

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -572,7 +572,7 @@ class OrderController extends CoreApiController
             $query->where(['o_user_id'=>$uid]);
         }
 
-//        $query->andWhere(['>', 'r_overdue_day', 0]); // 逾期天数>0，
+        $query->andWhere(['>', 'r_overdue_day', 0]); // 逾期天数>0，
 
         $data = $query->andFilterWhere(['<=', 'r_overdue_day', $days])->groupBy('r_id')->asArray()->all();
 
