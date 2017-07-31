0abccfa454822d1d0e8468352a521ce30ec9385d
haytoo <466594257@qq.com>
2017-07-31 21:50:20 +0800

    test

--- sqlupdate.php
+++ sqlupdate.php
@@ -52,7 +52,7 @@ CREATE TABLE `yijifu_deduct` (
 `bankCode` varchar(16) NOT NULL DEFAULT '' COMMENT '银行编码',
 `realRepayTime` int(11) NOT NULL DEFAULT '0' COMMENT '实际还款时间',
 `status` tinyint(4) NOT NULL COMMENT '代扣状态：0等待异步回调 1待处理 2代扣处理中 3待审核 4审核驳回 5 代扣失败 6代扣成功 7结算成功 8接口调用失败',
-`errorCode` varchar(10) NOT NULL DEFAULT '' COMMENT '出错时的错误编码',
+`errorCode` varchar(20) NOT NULL DEFAULT '' COMMENT '出错时的错误编码',
 `description` varchar(128) NOT NULL DEFAULT '' COMMENT '错误描述',
 `repayment_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '待还款id',
 `operator_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '操作者id',
