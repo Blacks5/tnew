750fa9b59cfbefe66d2ae924bfd873de470963a2
haytoo <466594257@qq.com>
2017-08-09 23:13:21 +0800

    修改统计代码

---
+++ wechat/controllers/WechatController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: too
+ * Date: 2017/8/9
+ * Time: 23:01
+ * @author too <hayto@foxmail.com>
+ */
+
+namespace wechat\controllers;
+
+
+use EasyWeChat\Foundation\Application;
+use yii\web\Controller;
+
+class WechatController extends Controller
+{
+    public function actionIndex()
+    {
+        $config = \Yii::$app->params['wechat'];
+        $app = new Application($config);
+
+        $app->server->setMessageHandler(function($message){
+            return "您好！欢迎关注天牛金融!";
+        });
+        $response = $app->server->serve();
+
+        $response->send();
+    }
+}
