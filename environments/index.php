9500272ae03159850f83ea490b9df4e556dcf76d
haytoo <466594257@qq.com>
2017-08-09 23:36:02 +0800

    修改统计代码

--- environments/index.php
+++ environments/index.php
@@ -38,6 +38,8 @@ return [
             'frontend/web/assets',
             'api/runtime',
             'api/web/assets',
+            'wechat/runtime',
+            'wechat/web/assets',
         ],
         'setExecutable' => [
             'yii',
@@ -47,6 +49,7 @@ return [
             'backend/config/main-local.php',
             'frontend/config/main-local.php',
             'api/config/main-local.php',
+            'wechat/config/main-local.php',
         ],
     ],
     'Production' => [
@@ -58,6 +61,8 @@ return [
             'frontend/web/assets',
             'api/runtime',
             'api/web/assets',
+            'wechat/runtime',
+            'wechat/web/assets',
         ],
         'setExecutable' => [
             'yii',
@@ -66,6 +71,7 @@ return [
             'backend/config/main-local.php',
             'frontend/config/main-local.php',
             'api/config/main-local.php',
+            'wechat/config/main-local.php',
         ],
     ],
 ];
