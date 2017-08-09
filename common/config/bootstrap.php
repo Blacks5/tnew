10eb154df497c00211acd51579b70f5f6399d89d
haytoo <466594257@qq.com>
2017-08-09 23:42:53 +0800

    修改统计代码

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,4 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@wechat', dirname(dirname(__DIR__)) . '/wechat');
