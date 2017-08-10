5ec49d34041e0486305afff550a63bc22a3f9900
haytoo <466594257@qq.com>
2017-08-10 22:53:58 +0800

    修改统计代码

--- wechat/controllers/WechatController.php
+++ wechat/controllers/WechatController.php
@@ -30,7 +30,65 @@ class WechatController extends Controller
 
             ob_start();
             var_dump($message);
-//            var_dump($userinfo);
+
+            $app->menu->destroy();
+            $buttons = [
+                [
+                    "type" => "click",
+                    "name" => "天",
+                    "key"  => "V1001_TODAY_MUSIC"
+                ],
+                [
+                    "name"       => "牛",
+                    "sub_button" => [
+                        [
+                            "type" => "view",
+                            "name" => "金融",
+                            "url"  => "http://www.baidu.com/"
+                        ],
+                        [
+                            "type" => "click",
+                            "name" => "赞一下我们",
+                            "key" => "V1001_GOOD"
+                        ]
+                    ],
+                ],
+            ];
+
+            $buttons_gx = [
+                [
+                    "type" => "click",
+                    "name" => "天1",
+                    "key"  => "V1001_TODAY_MUSIC"
+                ],
+                [
+                    "name"       => "牛1",
+                    "sub_button" => [
+                        [
+                            "type" => "view",
+                            "name" => "金融1",
+                            "url"  => "http://www.baidu.com/"
+                        ],
+                        [
+                            "type" => "click",
+                            "name" => "赞一下我们1",
+                            "key" => "V1001_GOOD"
+                        ]
+                    ],
+                ],
+            ];
+            $matchRule = [
+//                "tag_id"=>"2",
+                "sex"=>"1",
+                "country"=>"中国",
+                "province"=>"四川",
+                "city"=>"成都",
+//                "client_platform_type"=>"2",
+                "language"=>"zh_CN"
+            ];
+//            var_dump($app->menu->add($buttons_gx, $matchRule));
+//            var_dump($app->menu->add($buttons));
+//            var_dump($app->menu->destroy(415564445));
 
             $ret = ob_get_clean();
             return $ret;
