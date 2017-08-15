34a734088eb864190a14702f4960785d2b9d581d
haytoo <466594257@qq.com>
2017-08-15 21:38:11 +0800

    修改a标签href值

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -193,17 +193,16 @@ SIGN_SUCCESS：签约成功
      */
     public function actionSendws($a)
     {
-  //      $client = new Client("ws://192.168.0.194:8081");
-        $client = new Client(Yii::$app->params['ws']);
+        $client = new Client(\Yii::$app->params['ws']);
+        $string = '顾客:李大爷产生了新订单';
         $data = [
-            'cmd'=>'Orders:loanNotify',
+            'cmd'=>'Orders:newOrderNotify',
             'data'=>[
-                'message'=>"李大爷创建了新订单",
-                'order_id'=>5
+                'message'=>$string,
+                'order_id'=>1
             ]
         ];
         $jsonData = json_encode($data, JSON_UNESCAPED_UNICODE);
-        var_dump($jsonData);
         $client->send($jsonData);
     }
 
