9ef92d5fb9d4b4304a761ce3b8d3625bd04996a8
pixiaoshi <p304363979@163.com>
2017-09-05 09:47:58 +0800

    借款管理（新）也增加对应的取消个人保障和取消贵宾包操作

--- backend/views/borrow/view.php
+++ backend/views/borrow/view.php
@@ -546,10 +546,10 @@ $this->title = $model['c_customer_name'] . '借款详情【'. $msg. '】';
                         <button class="btn btn-danger" id="calculation_residual_loan">提前还款余额计算</button>
                         <button class="btn btn-danger col-md-offset-1">提前还款</button>
                         <?php }?>
-                        <?php if($model['o_is_add_service_fee'] == 1 && $model['o_status'] == 10 && ($model['o_operator_date'] - time()) < 3600*24*120){ ?>
+                        <?php if($model['o_is_add_service_fee'] == 1 && $model['o_status'] == 10 && (time() - $model['o_operator_date']) > 3600*24*120){ ?>
                         <button class="btn btn-danger col-md-offset-1" id="cancel_personal_protection">取消个人保障计划</button>
                         <?php }?>
-                        <?php if($model['o_is_free_pack_fee'] == 1 && $model['o_status'] == 10 && ($model['o_operator_date'] - time()) < 3600*24*120){ ?>
+                        <?php if($model['o_is_free_pack_fee'] == 1 && $model['o_status'] == 10 && (time() - $model['o_operator_date']) > 3600*24*120){ ?>
                         <button class="btn btn-danger col-md-offset-1" id="cancel_vip_pack">取消贵宾服务包</button>
                         <?php }?>
                     </div>
