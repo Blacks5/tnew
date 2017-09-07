8c9f3f2112c6fd8d66ed069f23254f6e090ad984
tn-mumu <tnmumu@qq.com>
2017-09-07 14:55:48 +0800

    调整修改微信端

--- wechat/views/site/index.php
+++ wechat/views/site/index.php
@@ -81,17 +81,19 @@
     $(function() {
         FastClick.attach(document.body);
 
-        wx.config(<?php echo $js->config(array('hideMenuItems'), true) ?>);
+        wx.config(<?php echo $js->config(['hideMenuItems'], true) ?>);
 
-        wx.hideMenuItems({
-            menuList: [
-                'menuItem:share:appMessage',
-                'menuItem:share:timeline',
-                'menuItem:share:qq',
-                'menuItem:share:weiboApp',
-                'menuItem:share:facebook',
-                'menuItem:share:QZone',
-            ]
+        wx.ready(function(){
+            wx.hideMenuItems({
+                menuList: [
+                    'menuItem:share:appMessage',
+                    'menuItem:share:timeline',
+                    'menuItem:share:qq',
+                    'menuItem:share:weiboApp',
+                    'menuItem:share:facebook',
+                    'menuItem:share:QZone',
+                ]
+            });
         });
     });
 </script>
