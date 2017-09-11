0345c847c796d013b7aeb820aca898a342fc6c1f
jetking <jetking@qq.com>
2017-09-11 17:08:45 +0800

    增加日志:修改银行卡回调

--- backend/config/operation-log.php
+++ backend/config/operation-log.php
@@ -4,6 +4,6 @@ return [
         'customer/view' => ['tag' => 'customer.view', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]查看了客户'],
         // 'customer/index' => ['tag' => 'customer.index', 'active' => 'test'],
         'borrownew/deduct-callback' => ['tag' => 'api.callback.deduct', 'memo' => 'API回调:易极付-扣款回调'],
-        'borrow/update-bank-call-back' => ['tag' => 'api.callback.', 'memo' => 'API回调:易极付-修改银行卡'],
+        'borrow/update-bank-call-back' => ['tag' => 'api.callback.bankcard-update', 'memo' => 'API回调:易极付-修改银行卡'],
     ],
 ];
