746fa4cd3fd77ff7d4c93029384cb9450e493cd7
jetking <jetking@qq.com>
2017-09-11 16:57:56 +0800

    fix bug: log

--- backend/services/OperationLog.php
+++ backend/services/OperationLog.php
@@ -47,9 +47,9 @@ class OperationLog
         }
         $operator = \Yii::$app->getUser()->getIdentity();
         $varTab = [
-            '{OPERATOR_ID}' => $operator->id,
-            '{OPERATOR_USERNAME}' => $operator->username,
-            '{OPERATOR_REALNAME}' => $operator->realname,
+            '{OPERATOR_ID}' => $operator->id ?? 0,
+            '{OPERATOR_USERNAME}' => $operator->username ?? '',
+            '{OPERATOR_REALNAME}' => $operator->realname ?? '',
         ];
         if (!$this->data['memo']) {
             $this->data['memo'] = str_replace(array_keys($varTab), array_values($varTab), $this->config['memo']);
