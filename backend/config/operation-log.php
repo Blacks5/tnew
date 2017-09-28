2a945fb483bcd8b4b381918045896ab685d8bf4b
jetking <jetking@qq.com>
2017-09-28 15:59:28 +0800

    增加更多后台操作日志

--- backend/config/operation-log.php
+++ backend/config/operation-log.php
@@ -2,7 +2,8 @@
 return [
     'operation-log' => [
         // 'customer/index' => ['tag' => 'customer.index', 'active' => 'test'], // 自定义函数方式处理
-        'customer/view' => ['tag' => 'customer.view', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]查看了客户'],
+        
+        // 回调相关:
         'borrownew/deduct-callback' => ['tag' => 'api.callback.yijifu.deduct-new', 'memo' => 'API回调:易极付-扣款回调-NEW'],
         'borrow/deduct-callback' => ['tag' => 'api.callback.yijifu.deduct', 'memo' => 'API回调:易极付-扣款回调'],
         // 'borrow/update-bank-call-back' => ['tag' => 'api.callback.bankcard-update', 'memo' => 'API回调:易极付-修改银行卡'],
@@ -10,5 +11,23 @@ return [
         'borrownew/deduct-callback' => ['tag' => 'api.callback.yijifu.borrownew-deduct', 'memo' => 'API回调:易极付-提前还款扣款'],
         'loan/async' => ['tag' => 'api.callback.yijifu.loan-pay', 'memo' => 'API回调:易极付-给商家放款'],
         'jun/callback' => ['tag' => 'api.callback.junziqian.sign', 'memo' => 'API回调:君子签-签约'],
+
+        // 订单、审核相关:
+        'borrow/view' => ['tag' => 'borrow.view', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]查看了订单详情'],
+        'borrow/verify-pass-first' => ['tag' => 'borrow.verify-pass-first', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}] 初审通过'],
+        'borrownew/verify-pass' => ['tag' => 'borrownew.verify-pass', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}] 终审放款'],
+        'borrow/verify-cancel' => ['tag' => 'borrow.verify-cancel', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}] 取消订单'],
+        'borrow/verify-refuse' => ['tag' => 'borrow.refuse', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]拒绝订单'],
+        'borrow/verify-failpic' => ['tag' => 'borrow.verify-failpic', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]审核-照片不合格'],
+
+        // 提前还款、取消贵宾服务包、取消个人保障计划:
+        'borrownew/prepayment' => ['tag' => 'borrownew.prepayment', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]- 提前还款'],
+        'borrownew/cancel-vip-pack' => ['tag' => 'borrownew.cancel-vip-pack', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]取消贵宾服务包'],
+        'borrownew/cancel-personal-protection' => ['tag' => 'borrownew.cancel-personal-protection', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}]取消个人保障计划'],
+
+        // ..
+        'loan/loan' => ['tag' => 'loan.loan', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}] 发起向商家放款请求'],
+        'repaymentnew/repay' => ['tag' => 'repaymentnew.repay', 'memo' => '{OPERATOR_REALNAME}[{OPERATOR_ID}] 发起还款扣款请求'],
+
     ],
 ];
