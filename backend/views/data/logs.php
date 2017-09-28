ec57adf25e1d30a66274c1c9b50532cd18ba1414
一步之瑶 <wuya520123@vip.qq.com>
2017-09-28 16:54:32 +0800

    增加日志

--- backend/views/data/logs.php
+++ backend/views/data/logs.php
@@ -1,7 +1,65 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 2017/9/28
- * Time: 15:21
- */
+<link href="/css/plugins/datapicker/datepicker3.css" rel="stylesheet">
+<script src="/js/plugins/datapicker/bootstrap-datepicker.js"></script>
+
+<div class="ibox-content">
+    <div class="row">
+        <div class="col-sm-12">
+            <form method="get" class="row" action="">
+                <div class="col-sm-1">
+                    <input type="text" class="form-control" name="DataSearch[realname]" value="<?= $sear['realname'] ?>" placeholder="操作人姓名">
+                </div>
+                <div class="col-sm-2">
+                    <select class="form-control" name="DataSearch[typeTag]">
+                        <option value="">选择日志类型</option>
+                        <?php foreach ($type as $k => $v){ ?>
+                            <option value="<?= $v[0] ?>" <?= $v[0]==$sear['typeTag']?' selected':'' ?> ><?= $v[1] ?></option>
+                        <?php }?>
+                    </select>
+                </div>
+                <div class="col-sm-3">
+                    <div class="input-daterange input-group" id="datepicker">
+                        <input type="text" class="form-control" name="DataSearch[start_time]"
+                               value="<?= $sear['start_time']? date('Y-m-d', $sear['start_time']): ''; ?>" placeholder="开始时间">
+                        <span class="input-group-addon ">到</span>
+                        <input type="text" class="form-control" name="DataSearch[end_time]"
+                               value="<?= $sear['end_time'] ? date('Y-m-d', $sear['end_time']):''; ?>" placeholder="结束时间">
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-info"><i class="fa fa-search"></i>查询</button>
+                    </div>
+                </div>
+            </form>
+            <script>
+                $('#datepicker').datepicker({
+                    todayBtn: "linked",
+                    keyboardNavigation: true,
+                    forceParse: true,
+                    autoclose:true,
+                    format: "yyyy-mm-dd",
+                    todayHighlight: true
+                });
+            </script>
+        </div>
+    </div>
+    <div class="row">
+        <div class="list-group">
+            <?php foreach ($data as $k => $v){ ?>
+            <a class="list-group-item">
+                <h4 class="list-group-item-heading"><?= $v['memo'] ?></h4>
+                <p>操作员:<?=$v['realname']?> 操作时间:<?= $v['created_at']?> &nbsp;&nbsp;操作IP:<?= $v['ip']?> &nbsp;&nbsp; 关联订单ID:<?= $v['order_id']?></p>
+                <p id="info" style="display:none;"><code><?= $v['data'] ?></code></p>
+            </a>
+            <?php }?>
+        </div>
+    </div>
+</div>
+<script>
+    $('.list-group-item').mousemove(function(){
+        $(this).find('#info').show();
+    })
+    $('.list-group-item').mouseout(function(){
+        $(this).find('#info').hide();
+    })
+</script>
