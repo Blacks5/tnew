0a824fe1a39e68b7bc89d28cd08d47acac53666f
tn-mumu <tnmumu@qq.com>
2017-09-29 13:47:37 +0800

    添加表单csrf禁用功能

--- backend/core/CoreBackendController.php
+++ backend/core/CoreBackendController.php
@@ -8,22 +8,33 @@
  */
 namespace backend\core;
 
-use yii;
-use common\core\CoreCommonController;
 use backend\services\OperationLog;
+use common\core\CoreCommonController;
+use yii;
 
-class CoreBackendController extends CoreCommonController
-{
-    public function beforeAction($action)
-    {
-        Yii::$app->getView()->title = '天牛金融--后台';
-        $log = new OperationLog();
-        $log->active();
-        return parent::beforeAction($action);
-    }
+class CoreBackendController extends CoreCommonController {
+	public function beforeAction($action) {
+		Yii::$app->getView()->title = '天牛金融--后台';
 
-    public function afterAction($action, $result)
-    {
-        return parent::afterAction($action, $result);
-    }
+		$controllerName = $action->controller->id;
+
+		$actionName = $action->id;
+
+		$router = $controllerName . '/' . $actionName;
+
+		$enable_arr = \Yii::$app->params['enable_csrf_validation'];
+
+		if (in_array($router, $enable_arr)) {
+			$this->enableCsrfValidation = false;
+		}
+
+		$log = new OperationLog();
+		$log->active();
+
+		return parent::beforeAction($action);
+	}
+
+	public function afterAction($action, $result) {
+		return parent::afterAction($action, $result);
+	}
 }
