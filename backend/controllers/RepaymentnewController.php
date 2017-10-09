50efdb259459b3b1a9679c9702966530ac06314f
一步之瑶 <wuya520123@vip.qq.com>
2017-10-09 15:08:31 +0800

    代还列表一个订单只显示一条数据

--- backend/controllers/RepaymentnewController.php
+++ backend/controllers/RepaymentnewController.php
@@ -56,10 +56,9 @@ class RepaymentnewController extends CoreBackendController
     {
         $this->getView()->title = '待还款列表';
         $model = new RepaymentSearch();
-        $query = $model->repaymenlist(Yii::$app->getRequest()->getQueryParams());
+        $query = $model->repaymentListByOrders(Yii::$app->getRequest()->getQueryParams());
 //        $time = $_SERVER['REQUEST_TIME']+(3600*24*33);
-        $query = $query->andWhere(['r_status' => [Repayment::STATUS_NOT_PAY, Repayment::STATUS_PROCESSING]])
-            ->andWhere(['<', 'r_overdue_day', 30]);
+        $query = $query->andWhere(['r_status' => [Repayment::STATUS_NOT_PAY, Repayment::STATUS_PROCESSING]]);
         $query = $query->andWhere(['>=','o_created_at',strtotime(Yii::$app->params['customernew_date'])]);
         $querycount = clone $query;
         $pages = new yii\data\Pagination(['totalCount' => $querycount->count()]);
