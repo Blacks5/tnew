4baa0f2107b624ffeceaf0cee0b782cdb472f2c4
OneStep <wuya520123@vip.qq.com>
2017-11-06 18:01:59 +0800

    !109 修改常规和促销的商户服务费 Merge pull request !109 from 王小帅/develop

---
+++ backend/controllers/CashExamineController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2017/11/3
+ * Time: 9:32
+ */
+
+namespace backend\controllers;
+
+
+use backend\core\CoreBackendController;
+
+class CashExamineController extends CoreBackendController
+{
+    public function actionWait()
+    {
+        $examine = 'wait';
+        return $this->render('wait',['examine' => $examine]);
+    }
+
+    public function actionRevoke()
+    {
+        $examine = 'revoke';
+        return $this->render('wait',['examine' => $examine]);
+    }
+
+    public function actionPass()
+    {
+        $examine = 'pass';
+        return $this->render('wait', ['examine' => $examine]);
+    }
+
+    public function actionInfo($id)
+    {
+        return $this->render('info', ['id' => $id]);
+    }
+}
