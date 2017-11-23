7cfa3481967f405bf12aa2315a1dd7e12c7e3ee8
OneStep <wuya520123@vip.qq.com>
2017-11-23 16:21:21 +0800

    完成第三方的列表

--- backend/views/cash-api/contract.php
+++ backend/views/cash-api/contract.php
@@ -1,7 +1,113 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 2017/11/17
- * Time: 16:50
- */
+<script src="/js/vue.js"></script>
+<script src="/js/vue-resource.js"></script>
+<script src="/js/plugins/layer/layer.min.js"></script>
+
+
+<div class="ibox-content" id="list">
+    <div class="row">
+        <div class="clients-list">
+            <div class="tab-content">
+                <div id="tab-1" class="tab-pane active">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th class="client-avatar">订单号</th>
+                                <th>签约ID</th>
+                                <th><a data-toggle="tab" href="#contact-3" class="client-link">客户姓名</a></th>
+                                <th>签约时间</th>
+                                <th>状态</th>
+                                <th>操作人</th>
+                                <th>创建时间</th>
+                                <th>操作</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="value in lists">
+                                <td class="client-avatar">{{value.order_id}}</td>
+                                <td>{{value.thirdparty_id}}</td>
+                                <td>{{value.identification_card.name}}</td>
+                                <td>{{value.signed_at ==null?'还未签约':value.signed_at }}</td>
+                                <td>
+                                    <a class="btn btn-xs btn-info" v-if="value.status =='pending'">等待签约</a>
+                                    <a class="btn btn-xs btn-success" v-if="value.status == 'signed'">签约成功</a>
+                                    <a class="btn btn-xs btn-danger" v-if="value.status == 'refused'">签约失败</a>
+                                </td>
+                                <td>{{value.name}}</td>
+                                <td>{{value.created_at}}</td>
+                                <td class="client-status">
+                                    <a class="btn btn-info btn-xs" @click="info(value.order_id)">详情</a>
+                                </td>
+                                <td>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--分页-->
+                    <ul class="pagination" v-if="pageCount != 1">
+                        <li>
+                            <a  aria-label="Previous" @click="pageLeft">
+                                <span aria-hidden="true">&laquo;</span> 上一页
+                            </a>
+                        </li>
+                        <li><a :value="pageIndex" >当前第 {{pageIndex}} 页</a></li>
+                        <li><a>共 {{pageCount}} 页</a></li>
+                        <li>
+                            <a  aria-label="Next" @click="pageRight">
+                                下一页 <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    Vue.use(VueResource);
+    new Vue({
+        el: '#list',
+        data:{
+            lists:[],
+            token: window.sessionStorage.getItem('V2_TOKEN'),
+            baseUrl:"<?= Yii::$app->params['cashBaseUrl'] ?>",
+            pageIndex:1,
+            pageCount:1
+        },
+        created: function () {
+            this.toSearch();
+        },
+        methods: {
+            toSearch:function (){
+                var url = this.baseUrl + "contract";
+                var header = {headers:{'X-TOKEN':this.token}};
+                this.$http.get(url,header).then(function (data){
+                    var json = data.bodyText;
+                    var usedData = JSON.parse(json);
+
+                    this.lists = usedData['data']['data'];
+                    this.pageCount = usedData['data']['last_page'];
+                    this.pageIndex = usedData['data']['current_page'];
+                },function (response){
+                    console.log(response['body']['errors']);
+                    layer.msg(response['body']['errors'][0]['message'],{icon:2})
+                })
+            },
+            info:function(id){
+                var url = this.baseUrl + id +"/contract";
+                var header ={headers:{'X-TOKEN':this.token}};
+                this.$http.get(url,header).then(function (data){
+                    var json =data.bodyText;
+                    var usedData = JSON.parse(json);
+
+                    window.op;
+                },function (response){
+                    layer.msg(response['body']['errors'][0]['message'],{icon:2});
+                })
+            }
+        }
+    });
+</script>
--- backend/views/cash-api/info.php
+++ backend/views/cash-api/info.php
@@ -1,7 +1,73 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 2017/11/20
- * Time: 11:17
- */
+<script src="/js/vue.js"></script>
+<script src="/js/vue-resource.js"></script>
+<script src="/js/plugins/layer/layer.min.js"></script>
+<style>
+    body{background-color: white;}
+    .height{height:60px;padding:20px 0;}
+</style>
+<div class="ibox float-e-margins" style="background-color: white">
+    <div class="ibox-content">
+        <div class="form-horizontal m-t" id="signupForm" novalidate="novalidate">
+            <div class="container-f" id="list">
+                <h1 class="text-center" v-if="url == 'deduct-signs'">{{json(data.bank_card)['user_name']}}的放款详情</h1>
+                <h1 class="text-center" v-if="url == 'loans'">{{json(data.bank_card)['user_name']}}的放款详情</h1>
+                <h1 class="text-center" v-if="url == 'deducts'">{{json(data.bank_card)['user_name']}}的放款详情</h1>
+                <div class="row">
+                    <div class="list-group">
+                        <a class="list-group-item col-sm-4">订单编号<span class="badge">{{data.order_number}}</span></a>
+                        <a class="list-group-item col-sm-4">第三方ID<span class="badge">{{data.thirdparty_id}}</span></a>
+                        <a class="list-group-item col-sm-4">借款人姓名<span class="badge">{{json(data.bank_card)['user_name']}}</span></a>
+                        <a class="list-group-item col-sm-4">身份证号<span class="badge">{{json(data.identification_card)['number']}}</span></a>
+                        <a class="list-group-item col-sm-4">银行卡号码<span class="badge">{{json(data.bank_card)['number']}}</span></a>
+                        <a class="list-group-item col-sm-4">手机号码<span class="badge">{{data.phone}}</span></a>
+                        <a class="list-group-item col-sm-4">操作人<span class="badge">{{data.name}}</span></a>
+                        <a class="list-group-item col-sm-4">借款金额<span class="badge">{{data.accepted_amount}}</span></a>
+                        <a class="list-group-item col-sm-4">状态<span class="badge">{{data.signStatus}}</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    Vue.use(VueResource);
+    new Vue({
+       el: '#list',
+       data:{
+           baseUrl:"<?= Yii::$app->params['cashBaseUrl'] ?>",
+           token: window.sessionStorage.getItem('V2_TOKEN'),
+           data:[],
+           url: "<?= $url ?>"
+       },
+       created: function (){
+           var url = this.baseUrl + "<?= $id ?>/<?= $url ?>";
+           var header = {
+               headers:{'X-TOKEN':this.token},
+               params: {
+
+               }
+
+           };
+
+           this.$http.get(url, header).then(function (data) {
+               var json = data.bodyText;
+               var usedData = JSON.parse(json);
+
+               this.data = usedData['data'];
+           }, function (response) {
+               console.log(response['body']['errors']);
+               layer.msg(response['body']['errors'][0]['message'], {icon: 2})
+           });
+       },
+        methods: {
+           json: function (data){
+               var isJson = typeof(data) == "object" && Object.prototype.toString.call(data).toLowerCase() == "[object object]" && !data.length;
+               if(isJson){
+                   return data;
+               }else{
+                   return JSON.parse(data);
+               }
+           }
+        }
+    });
+</script>
