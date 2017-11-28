14b9ef1a415e170dc853481451dcee50dfc94547
tn-mumu <tnmumu@qq.com>
2017-11-28 10:30:33 +0800

    修复禁用后仍然能够提交订单BUG

---
+++ wechat/views/site/fail.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>天牛金融管理</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="/wechat/lib/weui.min.css">
+    <link rel="stylesheet" href="/wechat/css/jquery-weui.css">
+    <link rel="stylesheet" href="/wechat/css/core.css">
+</head>
+<body ontouchstart>
+    <div class="main-box">
+        <div class="weui-msg">
+            <div class="weui-msg__icon-area"><i class="weui-icon-success weui-icon_msg"></i></div>
+            <div class="weui-msg__text-area">
+                <h2 class="weui-msg__title">账号不存在</h2>
+                <p class="weui-msg__desc">账号已禁用或不存在，请联系管理员处理。</p>
+            </div>
+            <div class="weui-msg__extra-area">
+                <div class="weui-footer">
+                    <p class="weui-footer__links">
+                        <a href="http://tnew.cn" class="weui-footer__link">天牛金融</a>
+                    </p>
+                    <p class="weui-footer__text">Copyright © 2017 tnew.cn</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
