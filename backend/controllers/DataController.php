0e97f99844e5ec8dee8b56d67c171e494aec2fe9
jetking <jetking@qq.com>
2017-12-18 10:42:32 +0800

    增加工具函数： 重新签约易极付，用于修改老版签约金额错误的问题

--- backend/controllers/DataController.php
+++ backend/controllers/DataController.php
@@ -104,4 +104,42 @@ class DataController extends CoreBackendController
        $list = $data->getCustomerOders($s_time, $e_time);
     }
 
+    /**
+     * actionChangeSign 重新签约易极付
+     * 用于修复旧版本中老订单签约时签约金额错误的问题（只签约了1个月）
+     * 保留此函数以便需要时使用
+     *
+     * @return void
+     */
+    public function actionChangeSign_NO_EXECUTE_b34f1bb78d4624e1df6678deb21a2ff8cdf1d241()
+    {
+        $oldSign = YijifuSign::find()
+            ->select([
+                'yijifu_sign.*',
+                'customer.c_customer_name',
+                'customer.c_customer_id_card',
+                'customer.c_banknum',
+                'customer.c_customer_cellphone',
+                'order_images.oi_after_contract',
+                'goods.g_goods_name',
+                'goods.g_goods_models',
+                'orders.o_total_price',
+                'orders.o_total_deposit',
+                'orders.o_service_fee',
+                'orders.o_inquiry_fee',
+                'orders.o_id'
+            ])
+            ->leftJoin(Orders::tableName(), 'orders.o_serial_id=yijifu_sign.o_serial_id')
+            ->leftJoin(Customer::tableName(),'o_customer_id=c_id')
+            ->leftJoin(OrderImages::tableName(), 'oi_id=orders.o_images_id')
+            ->leftJoin(Goods::tableName(), 'g_order_id=o_id')
+            ->where(['yijifu_sign.o_serial_id' => '17101200000003863302'])
+            ->andWhere(['status'=>1])
+            ->andWhere(['o_status'=> 10])
+            ->asArray()->all();
+        echo '待重新签约记录数：' . count($oldSign) . PHP_EOL;
+        // var_dump($oldSign);die();
+        $change = new ReturnMoney();
+        $isChange = $change->changeYijifuSign($oldSign);
+    }
 }
